3b1d87a4e8bbd9ea3a35946bc74429a7edc76dbb
Francis Hamzagic <francishamzagic@gmail.com>
2024-04-01 01:22:45 -0300

    basic structure and functionality

--- helpers.php
+++ helpers.php
@@ -16,11 +16,12 @@
   * @param string $name
   * @return void
   */
-  function loadView($name)
+  function loadView($name, $data = [])
   {
     $viewPath = basePath("views/{$name}.view.php");
     if(file_exists($viewPath))
     {
+      extract($data);
       require $viewPath;
     } else {
       echo "View {$name} does not exist";
@@ -65,4 +66,14 @@
     echo "<pre>";
     die(var_dump($value));
     echo "</pre>";
+  }
+
+  /**
+   * Format salary
+   * @param string $salary
+   * @return string Formatted Salary
+   */
+  function formatSalary($salary)
+  {
+    return '$' . number_format($salary);
   }
